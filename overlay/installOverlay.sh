b2e2ea17b00eebbbb0fda070d7e397fb3c593dfe
Saak Dertadian <root@arm.(none)>
2013-12-18 01:08:27 +0000

    Functional but not auto start

--- overlay/installOverlay.sh
+++ overlay/installOverlay.sh
@@ -4,9 +4,9 @@ export SLOTS=/sys/devices/bone_capemgr.9/slots
 export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
 
 CURRENT_DIR=$(pwd)
-
+	
 cd /lib/firmware
 echo Install Overlay...
-echo SAAK-Oled > $SLOTS
+echo BB-OLED > $SLOTS
 
 #cd $CURRENT_DIR
