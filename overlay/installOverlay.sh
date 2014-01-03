1cb382c96d1ffa6671d06cdc84cd6e3754dbe37a
Saak Dertadian <root@arm.(none)>
2014-01-03 00:50:37 +0000

    Start...

---
+++ overlay/installOverlay.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export SLOTS=/sys/devices/bone_capemgr.9/slots
+export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
+
+CURRENT_DIR=$(pwd)
+
+cd /lib/firmware
+echo Install Overlay...
+echo SAAK-Oled28 > $SLOTS
+
+#cd $CURRENT_DIR
