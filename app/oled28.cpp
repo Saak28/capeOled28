c472cbd642a87af621c12f3d7390695e15537dc6
Saak Dertadian <s.dertadian@gmail.com>
2014-01-02 22:51:49 -0800

    First functional test

--- app/oled28.cpp
+++ app/oled28.cpp
@@ -6,9 +6,12 @@
 #include <string>
 #include <unistd.h>
 #include <fcntl.h>
-//#include <poll.h>
+#include "SimpleGPIO.h"
 #include "oled28.h"
 using namespace std;
+
+unsigned int GPIO_DC=49;
+unsigned int GPIO_RS=60;
 
 int main(int argc, char *argv[])
 {
@@ -16,6 +19,22 @@ int main(int argc, char *argv[])
 
 	InitGpio();
 	OledInit();
+	OledCls(0x44);
+	usleep(1000000);
+	OledCls(0x00);
+	usleep(1000000);
+// 	OledCls(0xFF);
+// 	usleep(1000000);
+// 	OledWriteCmd(CMD_DISPLAY_MODE_INVERSE);	// Normal Display Mode (0x00/0x01/0x02/0x03)
+// 	OledWriteCmd(CMD_DISPLAY_MODE_ALL_ON);	// Normal Display Mode (0x00/0x01/0x02/0x03)
+	
+	for(int i=0;i<16;i++)
+		OledSetPixel(0,i,0x1000*i);
+
+	OledSetPixel(4,0,0xF0F0);
+	OledSetPixel(4,1,0x0F0F);
+	OledSetPixel(0,63,0x000F);
+
 
 	cout << "Init Oled 2.8 done..." << endl;
 	
@@ -26,17 +45,126 @@ int main(int argc, char *argv[])
 
 void InitGpio()
 {
+	gpio_export(GPIO_DC);
+	gpio_export(GPIO_RS);
+
+	gpio_set_dir(GPIO_DC,OUTPUT_PIN);
+	gpio_set_dir(GPIO_RS,OUTPUT_PIN);
+
+	gpio_set_value(GPIO_DC,HIGH);
+	gpio_set_value(GPIO_RS,HIGH);
 }
 
 void ReleaseGpio()
 {
+	gpio_unexport(GPIO_DC);
+	gpio_unexport(GPIO_RS);
 }
 
 int OledInit()
 {
-	WriteSpi(0x01);
+	gpio_set_value(GPIO_RS,LOW);
+	usleep(100000);
+	gpio_set_value(GPIO_RS,HIGH);
+
+	OledWriteCmd(CMD_SET_COMMAND_LOCK);
+	OledWriteData(0x12);	// Unlock Basic Commands (0x12/0x16)
 	
+	OledWriteCmd(CMD_SET_DISPLAY_OFF);	// Display Off (0x00/0x01)
+	
+	OledWriteCmd(CMD_SET_COLUMN_ADDRESS);
+	OledWriteData(0x1C);
+	OledWriteData(0x5B);
+	
+	OledWriteCmd(CMD_SET_ROW_ADDRESS);
+	OledWriteData(0x00);
+	OledWriteData(0x3F);
+	
+	OledWriteCmd(CMD_SET_DISPLAY_CLOCK);
+	OledWriteData(0x91);	// Set Clock as 80 Frames/Sec
+	
+	OledWriteCmd(CMD_SET_MULTIPLEX_RATIO);
+	OledWriteData(0x3F);	// 1/64 Duty (0x0F~0x3F)
+	
+	OledWriteCmd(CMD_SET_DISPLAY_OFFSET);
+	OledWriteData(0x00);	// Shift Mapping RAM Counter (0x00~0x3F)
+	
+	OledWriteCmd(CMD_SET_DISPLAY_START_LINE);
+	OledWriteData(0x00);	// Set Mapping RAM Display Start Line (0x00~0x7F)
+	
+	OledWriteCmd(CMD_SET_REMAP);
+	OledWriteData(0x14);	// 0x14
+	OledWriteData(0x11);	// Enable dual COM mode
+	
+	OledWriteCmd(CMD_SET_GPIO);
+	OledWriteData(0x00);	// Disable GPIO input pin
+	
+	OledWriteCmd(CMD_FUNCTION_SELECTION);
+	OledWriteData(0x01);	// Enable internal VDD regulator
+
+	OledWriteCmd(CMD_SET_DISPLAY_ENHANCEMENT_A);
+	OledWriteData(0xA0);	// Set_Display_Enhancement_A(0xA0,0xFD);
+	OledWriteData(0xFD);
+	
+	OledWriteCmd(CMD_SET_CONTRAST_CONTROL);
+	OledWriteData(0x9F);	// Set Segment Output Current
+	
+	OledWriteCmd(CMD_MASTER_CONTRAST_CONTROL);
+	OledWriteData(0x0F);	// Set Scale Factor of Segment Output Current Control 
+	
+	OledWriteCmd(CMD_SELECT_DEFAULT);	// set default linear gray scale table
+	OledWriteCmd(CMD_ENABLE_GRAYSCALE_TABLE);
+	
+	OledWriteCmd(CMD_SET_PHASE_LENGTH);
+	OledWriteData(0xE2);	// Set Phase 1 as 5 Clocks & Phase 2 as 14 Clocks
+
+	OledWriteCmd(CMD_SET_DISPLAY_ENHANCEMENT_B);
+	OledWriteData(0x20);	// Set_Display_Enhancement_B(0x20);
+	
+	OledWriteCmd(CMD_SET_PRECHARGE_VOLTAGE);
+	OledWriteData(0x1F);	// Set Pre‐Charge Voltage Level as 0.60*VCC
+	
+	OledWriteCmd(CMD_SET_SECOND_PRECHARGE_PERIOD);
+	OledWriteData(0x08);	// Set Second Pre‐Charge Period as 8 Clocks 
+	
+	OledWriteCmd(CMD_SET_VCOMH_VOLTAGE);
+	OledWriteData(0x07);	// Set Common Pins Deselect Voltage Level as 0.86*VCC
+	
+	OledWriteCmd(CMD_DISPLAY_MODE_NORMAL);	// Normal Display Mode (0x00/0x01/0x02/0x03)
+	
+	OledWriteCmd(CMD_EXIT_PARTIAL_DISPLAY);
+// 	OledWriteCmd(CMD_ENABLE_PARTIAL_DISPLAY);
+// 	OledWriteData(0x00);	// Disable Partial Display
+// 	OledWriteData(0x3F);	// 
+	
+	OledWriteCmd(CMD_SET_DISPLAY_ON);
+
 	return 1;
+}
+
+void OledCls(unsigned char fillData)
+{
+	OledWriteCmd(CMD_SET_COLUMN_ADDRESS);
+	OledWriteData(0x1C);
+	
+	OledWriteCmd(CMD_SET_ROW_ADDRESS);
+	OledWriteData(0x00);
+
+	OledWriteCmd(CMD_WRITE_RAM_CMD);
+	for(int i=0;i<(10*1024);i++)
+		OledWriteData(fillData);
+}
+
+int OledWriteCmd(unsigned char cmd)
+{
+	gpio_set_value(GPIO_DC,LOW);
+	return WriteSpi(cmd);
+}
+
+int OledWriteData(unsigned char data)
+{
+	gpio_set_value(GPIO_DC,HIGH);
+	return WriteSpi(data);
 }
 
 int WriteSpi(unsigned char data)
@@ -53,4 +181,17 @@ int WriteSpi(unsigned char data)
 	write(fd,buf,1);
 	close(fd);
 	return 1;
+}
+
+void OledSetPixel(unsigned char x,unsigned char y,unsigned int color)
+{
+	OledWriteCmd(CMD_SET_COLUMN_ADDRESS);
+	OledWriteData(0x1C+(x>>2));
+	
+	OledWriteCmd(CMD_SET_ROW_ADDRESS);
+	OledWriteData(y);
+	
+	OledWriteCmd(CMD_WRITE_RAM_CMD);
+	OledWriteData(color>>8);
+	OledWriteData(color);
 }
--- app/oled28.h
+++ app/oled28.h
@@ -4,11 +4,50 @@
 #define SYSFS_SPI_DIR		"/dev/spidev1.0"
 #define MAX_BUF				64
 
+///////////////////////////////////////////////////////
+// SSD1322 COMMANDS
+///////////////////////////////////////////////////////
+#define CMD_ENABLE_GRAYSCALE_TABLE					0x00
+#define CMD_SET_COLUMN_ADDRESS						0x15
+#define CMD_WRITE_RAM_CMD								0x5C
+#define CMD_READ_RAM_CMD								0x5D
+#define CMD_SET_ROW_ADDRESS							0x75
+#define CMD_SET_REMAP									0xA0
+#define CMD_SET_DISPLAY_START_LINE					0xA1
+#define CMD_SET_DISPLAY_OFFSET						0xA2
+#define CMD_DISPLAY_MODE_ALL_OFF						0xA4
+#define CMD_DISPLAY_MODE_ALL_ON						0xA5
+#define CMD_DISPLAY_MODE_NORMAL						0xA6
+#define CMD_DISPLAY_MODE_INVERSE						0xA7
+#define CMD_ENABLE_PARTIAL_DISPLAY					0xA8
+#define CMD_EXIT_PARTIAL_DISPLAY						0xA9
+#define CMD_FUNCTION_SELECTION						0xAB
+#define CMD_SET_DISPLAY_OFF							0xAE
+#define CMD_SET_DISPLAY_ON								0xAF
+#define CMD_SET_PHASE_LENGTH							0xB1
+#define CMD_SET_DISPLAY_CLOCK							0xB3
+#define CMD_SET_GPIO										0xB5
+#define CMD_SET_SECOND_PRECHARGE_PERIOD			0xB6
+#define CMD_SET_GRAYSCALE_TABLE						0xB8
+#define CMD_SELECT_DEFAULT								0xB9
+#define CMD_SET_PRECHARGE_VOLTAGE					0xBB
+#define CMD_SET_VCOMH_VOLTAGE							0xBE
+#define CMD_SET_CONTRAST_CONTROL						0xC1
+#define CMD_MASTER_CONTRAST_CONTROL					0xC7
+#define CMD_SET_MULTIPLEX_RATIO						0xCA
+#define CMD_SET_COMMAND_LOCK							0xFD
+#define CMD_SET_DISPLAY_ENHANCEMENT_A				0xB4
+#define CMD_SET_DISPLAY_ENHANCEMENT_B				0xD1
+
 int main(int argc,char *argv[]);
 void InitGpio(void);
 void ReleaseGpio(void);
 int OledInit(void);
+void OledCls(unsigned char);
+int OledWriteCmd(unsigned char);
+int OledWriteData(unsigned char);
 int WriteSpi(unsigned char);
+void OledSetPixel(unsigned char,unsigned char,unsigned int);
 
 char sysFsSpi[64];
 
